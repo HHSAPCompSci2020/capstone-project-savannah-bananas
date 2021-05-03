70126d95d57d2ba21964ef4cd50e3ece1f552fd2
jhwang04 <55427725+jhwang04@users.noreply.github.com>
2021-05-03 08:07:30 -0700

    Updated javadoc comment on the buttonPressed method

--- FrogDungeon/src/frog/util/ButtonListener.java
+++ FrogDungeon/src/frog/util/ButtonListener.java
@@ -4,6 +4,7 @@ public interface ButtonListener {
 
 	/**
 	 * Is called when a Button is pressed
+	 * @param button The Button that is pressed that calls this method
 	 */
 	public void buttonPressed(Button button);
 }
