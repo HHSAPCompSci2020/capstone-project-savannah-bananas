3aab72b47a74b757f2ac23d8fe8539c1952746d0
JMills20 <69985086+Jeremy-Mi@users.noreply.github.com>
2021-05-04 10:34:35 -0700

    Filled out the isTouching method for entities.

--- FrogDungeon/src/frog/weapons/Projectile.java
+++ FrogDungeon/src/frog/weapons/Projectile.java
@@ -2,4 +2,29 @@ package frog.weapons;
 
 public class Projectile {
 
+	//Fields
+	private double x, y;
+	private double range;
+	private double damage;
+	private double shootingSpeed;
+	
+	
+	
+	//Constructor
+	public Projectile(double x, double y, double damage, double range, double shootingSpeed) {
+		this.x = x;
+		this.y = y;
+		this.damage = damage;
+		this.range = range;
+		this.shootingSpeed = shootingSpeed;
+	}
+	
+	//Methods
+	public void move(double x, double y) {
+		
+	}
+	
+	public void moveTo(double tX, double tY) {
+		
+	}
 }
