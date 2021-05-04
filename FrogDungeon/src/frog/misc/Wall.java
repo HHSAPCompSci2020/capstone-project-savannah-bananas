9954797d4bb5c3ffea5d44fe08e429a08afe4961
jhwang04 <55427725+jhwang04@users.noreply.github.com>
2021-05-03 21:49:19 -0700

    Filled in Wall class

--- FrogDungeon/src/frog/misc/Wall.java
+++ FrogDungeon/src/frog/misc/Wall.java
@@ -22,6 +22,17 @@ public class Wall {
 	 */
 	private double x, y;
 	
+	/**
+	 * Width and height will usually be set to the default width and height
+	 */
+	private int width, height;
+	
+	/**
+	 * WALL_WIDTH should be greater than WALL_HEIGHT
+	 */
+	public static final int WALL_WIDTH = 400;
+	public static final int WALL_HEIGHT = 40;
+	
 	//Constructor
 	/**
 	 * Wall coordinates are weird. See example at
@@ -34,23 +45,75 @@ public class Wall {
 		this.x = x;
 		this.y = y;
 		this.type = WALL;
+		
+		if(isHorizontal()) {
+			this.width = WALL_WIDTH;
+			this.height = WALL_HEIGHT;
+		} else {
+			this.width = WALL_HEIGHT;
+			this.height = WALL_WIDTH;
+		}
 	}
 	
 	//Methods
 	/**
-	 * Returns an array of the points neighboring this tile.
-	 * @return Point[], the list of points around this tile.
+	 * Returns the Point of the tile on the opposite side of the wall. Returned Point (tile) may be out of bounds
+	 * @return Point of the tile on opposite side of the wall as the given one. If the given one does not neighbor the wall, then null is returned.
 	 */
-	public Point[] getNeighborTiles() {
-		return null;
+	public Point getTileOnOtherSide(Point p) {
+		if(!neighborsTile(p))
+			return null;
+		
+		if(isHorizontal()) {
+			if(p.y == this.y)
+				return new Point(p.x, p.y - 1);
+			else // if(p.y == this.y + 1)
+				return new Point(p.x, p.y + 1);
+		} else { // is vertical
+			if(p.x == this.x)
+				return new Point(p.x - 1, p.y);
+			else // if(p.x == this.x + 1)
+				return new Point(p.x + 1, p.y);
+		}
+	}
+	
+	/**
+	 * Returns true if the Point (tile coordinates) neighbors this wall, false if not
+	 */
+	private boolean neighborsTile(Point p) {
+		if(p.x == this.x || p.x == this.x + 1 || p.y == this.y || p.y == this.y + 1)
+			return true;
+		else
+			return false;
 	}
 	
 	/**
 	 * Draws this tile.
 	 * @param marker, the PApplet to draw this tile on.
+	 * @post marker ends with same style as it starts with
 	 */
 	public void draw(PApplet marker) {
+		marker.pushStyle();
+		marker.fill(0, 100, 0);
+		marker.noStroke();
 		
+		Point centerPoint = wallCoordsToPixelCoords(this.x, this.y);
+		marker.rect(centerPoint.x - width/2, centerPoint.y - height/2, width, height);
+			
+		marker.popStyle();
+	}
+	
+	/**
+	 * Returns Point that contains the pixel coordinates of the CENTER of the wall. NOT the upper left corner.
+	 * @param x X-coordinate (in wall coords) of the wall
+	 * @param y Y-coordinate (in wall coords) of the wall
+	 * @return Point containing pixel coordinates of the CENTER of the wall, NOT the upper left corner
+	 */
+	public static Point wallCoordsToPixelCoords(double x, double y) {
+		double newX = x * WALL_WIDTH;
+		double newY = x * WALL_HEIGHT;
+		
+		return new Point((int) newX, (int) newY);
 	}
 	
 	/**
