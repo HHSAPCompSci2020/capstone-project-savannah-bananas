0e537e498f558d65ba849096128958616f574ef9
mikaelakwan <69985036+mikaelakwan@users.noreply.github.com>
2021-05-04 21:42:56 -0700

    added gorf image to main screen

--- FrogDungeon/src/frog/screen/MenuScreen.java
+++ FrogDungeon/src/frog/screen/MenuScreen.java
@@ -1,13 +1,14 @@
 package frog.screen;
 
 import frog.util.Button;
-
+import processing.core.PImage;
 import frog.DrawingSurface;
 
 public class MenuScreen extends Screen {
 	//Fields
 	private Button startButton;
 	private Button infoButton;
+	private PImage gorf;
 	
 	//Constructors
 	/**
@@ -17,28 +18,35 @@ public class MenuScreen extends Screen {
 	public MenuScreen(DrawingSurface surface) {
 		super(surface);
 		
-		startButton = new Button(300, 150, 200, 150);
+		gorf = new PImage();
+		
+		startButton = new Button(300, 250, 200, 150);
 		startButton.setText("Start");
 		startButton.setButtonListener(this);
 		buttons.add(startButton);
 		
-		infoButton = new Button(300, 300, 200, 150);
+		infoButton = new Button(300, 400, 200, 150);
 		infoButton.setText("How To Play");
 		infoButton.setButtonListener(this);
 		buttons.add(infoButton);
 
 	}
 	
-	//Methods
 	public void draw() {
+		gorf = surface.loadImage("gorf1.png");
+
+
 		surface.background(0);
 		surface.pushStyle();
 		//surface.textAlign(DrawingSurface.CENTER, DrawingSurface.CENTER);
-		surface.text("FROG DUNGEON", 350, 100);
+
+		surface.text("FROG DUNGEON", 350, 50);
 		
 		updateButtons(surface.assumedCoordinatesToActual(surface.mouseX, surface.mouseY), surface.mousePressed);
 		drawButtons(surface);
 		
+		surface.image(gorf, 335, 85);
+
 		surface.popStyle();
 	}
 
