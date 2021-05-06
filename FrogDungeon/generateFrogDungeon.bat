339df2799a1f8a935de9a9b7f2f65e37b519fd31
jhwang04 <55427725+jhwang04@users.noreply.github.com>
2021-05-06 10:59:15 -0700

    added jar and javadocs

---
+++ FrogDungeon/generateFrogDungeon.bat
@@ -0,0 +1,23 @@
+set package=frog
+set mainClass=frog.Main
+set fileName=FrogDungeon.jar
+set resources=..\resources
+
+cd bin
+
+dir
+
+mkdir -p libs
+
+copy ..\lib\processing_core.jar .\libs\processing_core.jar
+cd libs
+tar xf processing_core.jar
+echo d | xcopy /E /y processing ..\processing
+cd ..
+rmdir .\libs /s /q
+
+jar cvfe ..\dist\%fileName% %mainClass% %package% processing %resources%
+
+rmdir processing /s /q
+
+cd ..
